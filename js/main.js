1963eefc3cf3a9448c6749ad6f118b79afcc728b
Akshesh <akshesh.doshi@innoplexus.com>
2016-06-15 17:54:54 +0530

    Add support for time-zone

--- js/main.js
+++ js/main.js
@@ -3,6 +3,31 @@ var size_second = 0.85,
 	size_minute = 0.75,
 	size_hour = 0.55;
 
+var timezone_map = {
+	ist: 0,
+	gmt: -5.5
+}
+
+var time = new Date();
+var tz = 'ist';
+
+function initialise() {
+	update_time();
+	start_clock();
+}
+
+function update_time() {
+	offset = timezone_map[tz];
+	time = new Date(new Date().getTime() + offset * 3600 * 1000);
+}
+
+function changeTimezone(elem) {
+	tz = elem.value;
+    offset = timezone_map[tz];
+    update_time();
+    start_clock();
+}
+
 function needles(hand, size, angle, cx, cy){
 	x2 = cx + size*Math.cos((angle*Math.PI/180) - Math.PI/2);
 	y2 = cy + size*Math.sin((angle*Math.PI/180) - Math.PI/2);
@@ -11,14 +36,17 @@ function needles(hand, size, angle, cx, cy){
 	hand.setAttribute('y2', y2);
 }
 
-start_clock();
-setInterval(start_clock, 1000);
 function start_clock(){
 
 	var cx=200, cy=200, r=150;
-	var time = new Date();
+	// changeTimezone();
+	update_time();
+	// time = new Date();
 
 	needles(document.getElementById("second"), size_second*r, 6*time.getSeconds(), cx, cy);
 	needles(document.getElementById("minute"), size_minute*r, 6*time.getMinutes(), cx, cy);
 	needles(document.getElementById("hour"), size_hour*r, 30*time.getHours(), cx, cy);
 }
+
+initialise();
+setInterval(start_clock, 1000);
